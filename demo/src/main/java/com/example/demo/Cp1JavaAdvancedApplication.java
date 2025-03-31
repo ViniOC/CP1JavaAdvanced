9cd7dc35ee54d008b375a65161717141615c2064
vinioc <viniciusoliveiracoutinho@gmail.com>
2025-03-31 10:53:23 -0300

    first commit

---
+++ demo/src/main/java/com/example/demo/Cp1JavaAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp1JavaAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cp1JavaAdvancedApplication.class, args);
+	}
+
+}
