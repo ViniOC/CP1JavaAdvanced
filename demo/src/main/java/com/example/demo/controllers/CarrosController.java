a6e89927a1740ec7aab92be170a8961f673e8086
vinioc <viniciusoliveiracoutinho@gmail.com>
2025-03-31 15:06:48 -0300

    feat: criacao do patchmapping

--- demo/src/main/java/com/example/demo/controllers/CarrosController.java
+++ demo/src/main/java/com/example/demo/controllers/CarrosController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/carros")
@@ -40,6 +41,34 @@ public class CarrosController {
     public ResponseEntity<Carros> removeCarById(@PathVariable Long id){
         this.service.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Carros> addCarro(@RequestBody Carros carro){
+        Carros carroCreated = this.service.save(carro);
+        return new ResponseEntity<>(carroCreated, HttpStatus.CREATED);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Carros> updateCarro(@PathVariable Long id, @RequestBody Carros carro){
+        Carros databaseCarros = this.service.getById(id);
+        databaseCarros.setMarca( carro.getMarca());
+        databaseCarros.setModelo(carro.getModelo());
+        databaseCarros.setAno(carro.getAno());
+        databaseCarros.setPotencia(carro.getPotencia());
+        databaseCarros.setEconomia(carro.getEconomia());
+        databaseCarros.setTipo(carro.getTipo());
+        databaseCarros.setPreco(carro.getPreco());
+        this.service.update(databaseCarros);
+        return new ResponseEntity<>(databaseCarros, HttpStatus.OK);
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<Carros> patchCarro(@PathVariable Long id, @RequestBody Map<String, Object>updates){
+        Carros updateCarro = this.service.partialUpdate(id, updates);
+        if(updateCarro != null){
+            return new ResponseEntity<>(updateCarro, HttpStatus.OK);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/potencia")
